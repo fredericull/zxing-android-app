63e4c419a283837b405eca37169603a7ba5371b6
Frédéric ULL <frederic.ull@gmail.com>
2023-10-25 21:50:00 +0200

    Request for Camera Permission

--- android/src/com/google/zxing/client/android/CaptureActivity.java
+++ android/src/com/google/zxing/client/android/CaptureActivity.java
@@ -32,11 +32,13 @@ import com.google.zxing.client.android.result.ResultHandlerFactory;
 import com.google.zxing.client.android.result.supplement.SupplementalInfoRetriever;
 import com.google.zxing.client.android.share.ShareActivity;
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -88,6 +90,7 @@ public final class CaptureActivity extends Activity implements SurfaceHolder.Cal
   private static final String[] ZXING_URLS = { "http://zxing.appspot.com/scan", "zxing://scan/" };
 
   private static final int HISTORY_REQUEST_CODE = 0x0000bacc;
+  private static final int PERMISSION_REQUEST_CODE = 0x0001bacc;
 
   private static final Collection<ResultMetadataType> DISPLAYABLE_METADATA_TYPES =
       EnumSet.of(ResultMetadataType.ISSUE_NUMBER,
@@ -146,7 +149,7 @@ public final class CaptureActivity extends Activity implements SurfaceHolder.Cal
   @Override
   protected void onResume() {
     super.onResume();
-    
+
     // historyManager must be initialized here to update the history preference
     historyManager = new HistoryManager(this);
     historyManager.trimHistory();
@@ -219,7 +222,7 @@ public final class CaptureActivity extends Activity implements SurfaceHolder.Cal
             cameraManager.setManualCameraId(cameraId);
           }
         }
-        
+
         String customPromptMessage = intent.getStringExtra(Intents.Scan.PROMPT_MESSAGE);
         if (customPromptMessage != null) {
           statusView.setText(customPromptMessage);
@@ -257,7 +260,7 @@ public final class CaptureActivity extends Activity implements SurfaceHolder.Cal
     if (hasSurface) {
       // The activity was paused but not stopped, so the surface still exists. Therefore
       // surfaceCreated() won't be called, so init the camera here.
-      initCamera(surfaceHolder);
+      initCameraPermission(surfaceHolder);
     } else {
       // Install the callback and wait for surfaceCreated() to init the camera.
       surfaceHolder.addCallback(this);
@@ -284,7 +287,7 @@ public final class CaptureActivity extends Activity implements SurfaceHolder.Cal
       }
     }
   }
-  
+
   private static boolean isZXingURL(String dataString) {
     if (dataString == null) {
       return false;
@@ -419,7 +422,7 @@ public final class CaptureActivity extends Activity implements SurfaceHolder.Cal
     }
     if (!hasSurface) {
       hasSurface = true;
-      initCamera(holder);
+      initCameraPermission(holder);
     }
   }
 
@@ -516,10 +519,10 @@ public final class CaptureActivity extends Activity implements SurfaceHolder.Cal
 
   private static void drawLine(Canvas canvas, Paint paint, ResultPoint a, ResultPoint b, float scaleFactor) {
     if (a != null && b != null) {
-      canvas.drawLine(scaleFactor * a.getX(), 
-                      scaleFactor * a.getY(), 
-                      scaleFactor * b.getX(), 
-                      scaleFactor * b.getY(), 
+      canvas.drawLine(scaleFactor * a.getX(),
+                      scaleFactor * a.getY(),
+                      scaleFactor * b.getX(),
+                      scaleFactor * b.getY(),
                       paint);
     }
   }
@@ -680,11 +683,11 @@ public final class CaptureActivity extends Activity implements SurfaceHolder.Cal
         // Reformulate the URL which triggered us into a query, so that the request goes to the same
         // TLD as the scan URL.
         int end = sourceUrl.lastIndexOf("/scan");
-        String productReplyURL = sourceUrl.substring(0, end) + "?q=" + 
+        String productReplyURL = sourceUrl.substring(0, end) + "?q=" +
             resultHandler.getDisplayContents() + "&source=zxing";
         sendReplyMessage(R.id.launch_product_query, productReplyURL, resultDurationMS);
         break;
-        
+
       case ZXING_LINK:
         if (scanFromWebPageManager != null && scanFromWebPageManager.isScanFromWebPage()) {
           String linkReplyURL = scanFromWebPageManager.buildReplyURL(rawResult, resultHandler);
@@ -700,7 +703,7 @@ public final class CaptureActivity extends Activity implements SurfaceHolder.Cal
       ClipboardInterface.setText(resultHandler.getDisplayContents(), this);
     }
   }
-  
+
   private void sendReplyMessage(int id, Object arg, long delayMS) {
     if (handler != null) {
       Message message = Message.obtain(handler, id, arg);
@@ -708,6 +711,16 @@ public final class CaptureActivity extends Activity implements SurfaceHolder.Cal
         handler.sendMessageDelayed(message, delayMS);
       } else {
         handler.sendMessage(message);
+      }
+    }
+  }
+
+  private void initCameraPermission(SurfaceHolder surfaceHolder) {
+    if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+      initCamera(surfaceHolder);
+    } else {
+      if (!shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)) {
+        requestPermissions(new String[]{Manifest.permission.CAMERA}, PERMISSION_REQUEST_CODE);
       }
     }
   }
